925f24017b5d440d8c9f5eac8304ae6e05d540c4
SaiSawant1 <saisawant2003@gmail.com>
2024-10-25 19:33:42 +0530

    auth ready

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,5 +3,7 @@ CREATE TABLE user_account (
   id UUID PRIMARY KEY,
   name text,
   email text UNIQUE,
-  password text
+  password text,
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp
 );
